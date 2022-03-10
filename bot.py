8616605d65d40e3de5abe36632a2168a60416821
DanOlder <vost3656@yandex.ru>
2022-03-11 01:46:39 +0300

    Almost there 2

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@
 import numpy as np
 
 import logging, requests
+import time
 
+from selenium import webdriver
 from bs4 import BeautifulSoup
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -70,6 +72,8 @@ def site(update: Update, context: CallbackContext) -> None:
     FIND_CLASS = 'hy5 y5h'
     # parser lutshe pomenyat' !!!
     PARSER = 'lxml'
+    SLEEP_TIME_FOR_LOAD = 0.5
+    PAGES_TO_FIND = 10
 
 
 
@@ -86,46 +90,51 @@ def site(update: Update, context: CallbackContext) -> None:
             'user-agent': user_agent,
         }
 
-    r = requests.get(url, headers=headers)
 
-    soup = BeautifulSoup(r.text, PARSER)
+    ozon_code = context.args[0]
+    counter = 1
+    is_found = 0
 
-    #################check the html file##################
+
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    for page in range(1,PAGES_TO_FIND+1):
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        time.sleep(SLEEP_TIME_FOR_LOAD) # wait for html to be fully loaded
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        driver.quit()
+
+
+        for item in soup.find_all("div", {"class": FIND_CLASS}):
+            link = item.a.get('href')
+            if ozon_code in link:
+                is_found = 1
+                break
+            counter += 1
+
+        if is_found:
+            update.message.reply_text(f"Позиция в поиске: {counter}\n Номер страницы с товаром: {page}")
+            break
+
+
+
     
-    f = open("ozon html.txt", "w", encoding="utf-8")
-    f.write(soup.prettify())
-    f.close()
-    
-    ######################################################
 
-    print("###############################################################")
-    for item in soup.find_all("div", {"class": FIND_CLASS}):
-        link = item.a.get('href')
-        print(link)
-        print("###############################################################")
+
+
+
 
 '''
     print("###############################################################")
+    #иногда классы меняются к хуям(не надежный способ поиска адреса)
     item = soup.find("div", {"class": FIND_CLASS})
     link = item.a.get('href')
     print(link)
     print("###############################################################")
-
-    for i in range(3):
-        print("###############################################################")
-        print('check regular counter')
-        item = item.findNext("div", {"class": FIND_CLASS})
-        link = item.a.get('href')
-        print(link)
-        print("###############################################################")
-
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    print('check regular counter')
-    #item = item.findNext("div", {"class": FIND_CLASS})
-    item = item.findNext("div", {"class": 'hy5 y5h'})
-    #link = item.a.get('href')
-    print(item)
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 '''
 '''
 
